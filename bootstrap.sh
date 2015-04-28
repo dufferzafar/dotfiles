8547d822d8f4749fe7a194310400c0eaa0845e7b
Shadab Zafar <dufferzafar0@gmail.com>
2015-04-28 21:02:52 +0530

    .gitignore and .gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
-# Get current folder
+# Get script's folder
 # http://stackoverflow.com/a/246128
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Zsh
+# Git
+ln -s $DIR/git/.gitconfig ~/.gitconfig
+ln -s $DIR/git/.gitignore ~/.gitignore
+
+exit
+########################################### The End
+
+# Zsh (I've started using Fish now)
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 rm ~/.zshrc
 ln -s $DIR/zsh/.zshrc ~/.zshrc
-
