d2228dac044bae87d96fafc9e1a456fef32391db
Shadab Zafar <dufferzafar0@gmail.com>
2015-06-10 10:24:58 +0530

    Updated the backup script

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,21 +5,34 @@ mkdir -p $dir
 # echo the command being executed
 set -x
 
-############################################################## /etc/
+############################################################## /
+
+# etc
+tar -cf $dir/etc.tar -C / etc
+
+# bin - folders in $PATH
+tar -cf $dir/bin.tar -C / bin sbin usr/bin usr/sbin usr/local/sbin usr/local/bin
 
 # Apache2
 tar -cf $dir/apache2.tar -C / etc/apache2 var/www
 
-# apt - some config, sources, and
-tar -cf $dir/apt.tar -C / etc/apt
-
-# bash completions
-tar -cf $dir/bash_completions.tar -C / etc/bash_completion.d
-
-# Misc Config Files
-tar -cf $dir/etc-config.tar -C / etc/wvdial.conf
-
-############################################################# /home/dufferzafar
+############################################################# $HOME
 
 # Desktop Files
-tar -cf $dir/desktop-files.tar -C / home/dufferzafar/.local/share/applications/
+home = home/dufferzafar
+tar -cf $dir/desktop-files.tar -C / $hom/.local/share/applications/
+
+# .cache - I am pretty stingy when it comes to downloaded stuff
+cache = $hom/.cache
+tar -cf $dir/home-cache.tar -C / $cache/bower $cache/pip $hom/.npm
+
+# Languages stuff
+tar -cf $dir/home-cache-languages.tar -C / $hom/.go $cache/.rvm $hom/.gem
+
+# .config
+tar -cf $dir/home-miscellany.tar -C / $hom/.purple $hom/.zotero $hom/nltk_data
+tar -cf $dir/home-config-others.tar -C / $hom/.vim $hom/.oh-my-zsh $hom/.dotfiles $hom/.ssh $hom/.fonts $hom/.net $hom/.js $hom/.css
+tar -cf $dir/home-config.tar -C / $hom/.config
+
+# Backup all 1 level files whose names start with '.'
+tar -cf $dir/home-files.tar $(find $HOME -maxdepth 1 -type f -name "\.*")
