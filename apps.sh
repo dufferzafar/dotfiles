8cc3f48412598a5764224500cb207de47983c1c8
Shadab Zafar <dufferzafar0@gmail.com>
2015-07-07 23:17:28 +0530

    Apps: Made an entry for nginx_ensite in apps.sh

--- apps.sh
+++ apps.sh
@@ -5,3 +5,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # Facebook Path Picker
 chmod +x $DIR/apps/fpp/fpp
 ln -sf $DIR/apps/fpp/fpp /usr/local/bin/fpp
+
+# nginx-ensite - similar to apache's a2ensite
+ln -sf "$DIR/apps/nginx_ensite/nginx_ensite" /usr/local/bin/nginx_ensite
+ln -sf "$DIR/apps/nginx_ensite/nginx_dissite" /usr/local/bin/nginx_dissite
+source "$DIR/apps/nginx_ensite/bash_completion.d/nginx_ensite"
