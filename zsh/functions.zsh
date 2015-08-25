faeb246cf015a1cf53488c0c2f022ca747b1e5b9
Shadab Zafar <dufferzafar0@gmail.com>
2015-08-25 20:23:14 +0530

    Zsh: Add a function to explain shell commands

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,6 +9,23 @@ google() {
     xdg-open "http://www.google.com/search?q=$search"
 }
 
+# Explain a shell command and it's params
+# https://www.mankier.com/blog/explaining-shell-commands-in-the-shell.html
+explain () {
+  if [ "$#" -eq 0 ]; then
+    while read  -p "Command: " cmd; do
+      curl -Gs "https://www.mankier.com/api/explain/?cols="75 --data-urlencode "q=$cmd"
+    done
+    echo "Bye!"
+  elif [ "$#" -eq 1 ]; then
+    curl -Gs "https://www.mankier.com/api/explain/?cols="75 --data-urlencode "q=$1"
+  else
+    echo "Usage"
+    echo "explain                  interactive mode."
+    echo "explain 'cmd -o | ...'   one quoted command to explain it."
+  fi
+}
+
 # File extractor
 # usage: extract <file>
 extract ()
