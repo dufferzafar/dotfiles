60faf53ea503aa8d80ca8db82fdb8d5c63a47507
Shadab Zafar <dufferzafar0@gmail.com>
2016-01-20 15:44:24 +0530

    Remove packages folder: moved to pam!

--- packages/list-npm-packages.sh
+++
@@ -1,6 +0,0 @@
-
-# npm list --global --depth=0
-# will print out specific versions too and we don't want them
-
-# List all npm packages
-ls "$(npm root --global)"
--- packages/npm-packages.sh
+++
@@ -1,7 +0,0 @@
-/usr/lib
-├── bower@1.4.1
-├── gulp@3.8.11
-├── less@2.4.0
-├── less-plugin-clean-css@1.5.0
-└── npm@1.4.28
-
