1785bab29181f501c5d220380cd23c731bb0f071
Shadab Zafar <dufferzafar0@gmail.com>
2016-01-20 15:26:11 +0530

    Remove backup folder: moved to pam!

--- backup/Readme.md
+++
@@ -1,3 +0,0 @@
-# Readme
-
-I will soon be moving to Kubuntu 15.04 but before that I need to backup up my stuff.
--- backup/daily.sh
+++
@@ -1,7 +0,0 @@
-# Todo: Send notifications when the job completes, or errors.
-# Delete backups older than 3 days!
-
-dir="/mnt/Stuff/Backup/Ubuntu"
-
-# Backup all 1 level files whose names start with '.'
-tar -cf $dir/home-files.tar "$(find "$HOME" -maxdepth 1 -type f -name "\.*")"
