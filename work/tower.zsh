e8d55c33dd48b67ed4e02b03914147dfe031e734
Shadab Zafar <dufferzafar0@gmail.com>
2022-08-22 09:53:20 +0530

    Tower custom zsh profile

--- work/tower.zsh
+++ work/tower.zsh
@@ -3,13 +3,30 @@
 # https://confluence.tower-research.com/display/DT/Quick+Set+up+for+Conda
 # source tpm-deactivate; source tpm-deactivate
 unset PYTHONPATH PYTHONHOME
-export PYTHONNOUSERSITE=True
 
-# This conda is managed by the devtools team
+# This is needed for conda environments but needs to disabled when using system python
+# export PYTHONNOUSERSITE=True
+
+# Devtools custom utilities & main profile (includes conda)
+# https://confluence.tower-research.com/display/DT/Defining+and+using+your+own+utilities
+export DEVTOOLS_CUSTOM_UTILS="
+utility=cgdb;spec=cgdb=0.8.0;public=true
+utility=gdb;spec=gdb=10;public=true
+utility=heaptrack;spec=heaptrack=1.4.0;public=true
+utility=heaptrack_print;spec=heaptrack=1.4.0;public=true
+utility=inotifywatch;spec=inotify-tools=3;public=true
+utility=mycli;spec=mycli=1.22;public=true
+utility=pgcli;spec=pgcli=3;public=true
+utility=zstd;spec=zstd>=1.5;public=true
+"
 source /opt/devtools/utils/etc/profile
+#utility=vd;spec=visidata=2.8;public=true
+#utility=mamba;spec=micromamba=0.24.0;binary=micromamba;public=true
+
+# This only brings in devtools conda and not other utilities
 # source /opt/devtools/conda/etc/profile.d/conda.sh
 
-# But I also have a separately installed conda in /spare/local/conda
+# Separately installed conda in /spare/local/conda - DEPRECATED
 # [[ -f /spare/local/conda/etc/profile.d/conda.sh ]] && source /spare/local/conda/etc/profile.d/conda.sh 2> /dev/null
 
 # This is the core environment
@@ -24,7 +41,7 @@ alias trc-fmt="trc-clang-format -i"
 # =========================================================
 
 # Binaries of 3rd party tools
-export PATH=/spare/ssd/szafar/apps/bin/:$PATH
+export PATH=/spare/local/szafar/apps/bin/:$PATH
 
 # JQ Helpers
 
